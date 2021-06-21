584e5e986f5e5b4b5b7e177d61d0fbca81dfc525
Pierre Ablin <pierreablin@gmail.com>
2021-06-21 23:19:47 +0200

    added solvers + benchmark

---
+++ solvers/picard.py
@@ -0,0 +1,26 @@
+import warnings
+from benchopt import BaseSolver, safe_import_context
+
+
+with safe_import_context() as import_ctx:
+    from picard import picard
+
+
+class Solver(BaseSolver):
+    """Picard"""
+    name = 'picard'
+
+    install_cmd = 'pip'
+    requirements = ['python-picard']
+
+    def set_objective(self, X, A):
+        self.X = X
+        self.A = A
+
+    def run(self, n_iter):
+        warnings.filterwarnings('ignore', category=UserWarning)
+        K, W, _ = picard(self.X, max_iter=n_iter + 1, tol=1e-5, ortho=False)
+        self.W = W @ K
+
+    def get_result(self):
+        return self.W
